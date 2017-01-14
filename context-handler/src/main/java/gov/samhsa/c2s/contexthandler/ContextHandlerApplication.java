7c1f68adb51d97432ff54fa1aa7510d52e1038cd
JiahaoLi <Jiahao.Li@feisystems.com>
2017-01-14 13:22:46 -0500

    Enable security scope for accessing management endpoint
    #12361 - As a developer, I can use a centralized configuration management tool

--- context-handler/src/main/java/gov/samhsa/c2s/contexthandler/ContextHandlerApplication.java
+++ context-handler/src/main/java/gov/samhsa/c2s/contexthandler/ContextHandlerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableResourceServer
 public class ContextHandlerApplication {
 
     public static void main(String[] args) {
