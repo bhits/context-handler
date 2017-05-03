e4968388d8b1fab37b4f85f8c99a1204e4bba6f7
Shruti Rao <shruti.rao@feisystems.com>
2017-05-03 15:08:35 -0400

    Get Consent from FHIR Server
    #13511 - As a developer, I will have used FHIR consent for data segmentation

--- context-handler/src/main/java/gov/samhsa/c2s/contexthandler/service/FhirServerPolicyProviderImpl.java
+++ context-handler/src/main/java/gov/samhsa/c2s/contexthandler/service/FhirServerPolicyProviderImpl.java
@@ -1,6 +1,11 @@
 package gov.samhsa.c2s.contexthandler.service;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.IGenericClient;
+import ca.uhn.fhir.rest.gclient.ReferenceClientParam;
+import ca.uhn.fhir.rest.gclient.TokenClientParam;
+import ca.uhn.fhir.validation.FhirValidator;
+import gov.samhsa.c2s.common.consentgen.ConsentBuilder;
 import gov.samhsa.c2s.common.consentgen.ConsentDto;
 import gov.samhsa.c2s.common.consentgen.ConsentGenException;
 import gov.samhsa.c2s.common.consentgen.IndividualProviderDto;
@@ -8,15 +13,17 @@ import gov.samhsa.c2s.common.consentgen.OrganizationalProviderDto;
 import gov.samhsa.c2s.common.log.Logger;
 import gov.samhsa.c2s.common.log.LoggerFactory;
 import gov.samhsa.c2s.contexthandler.service.dto.XacmlRequestDto;
+import gov.samhsa.c2s.contexthandler.service.exception.ConsentNotFound;
 import gov.samhsa.c2s.contexthandler.service.exception.MockFileReadException;
+import gov.samhsa.c2s.contexthandler.service.exception.MultiplePatientsFound;
 import gov.samhsa.c2s.contexthandler.service.exception.NoPolicyFoundException;
+import gov.samhsa.c2s.contexthandler.service.exception.PatientNotFound;
 import gov.samhsa.c2s.contexthandler.service.exception.PolicyProviderException;
-import gov.samhsa.c2s.common.consentgen.ConsentBuilder;
-
 import org.codehaus.jackson.map.ObjectMapper;
 import org.herasaf.xacml.core.policy.Evaluatable;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Consent;
+import org.hl7.fhir.dstu3.model.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,12 +40,18 @@ public class FhirServerPolicyProviderImpl implements PolicyProvider {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private final FhirContext fhirContext;
+    @Autowired
+    private FhirContext fhirContext;
 
     @Autowired
-    public FhirServerPolicyProviderImpl(ConsentBuilder consentBuilder, FhirContext fhirContext) {
+    private FhirValidator fhirValidator;
+
+    @Autowired
+    private IGenericClient fhirClient;
+
+    @Autowired
+    public FhirServerPolicyProviderImpl(ConsentBuilder consentBuilder) {
         this.consentBuilder = consentBuilder;
-        this.fhirContext = fhirContext;
     }
 
     @Override
@@ -102,8 +115,40 @@ public class FhirServerPolicyProviderImpl implements PolicyProvider {
     //temp method
     @Override
     public Bundle tempGetFhirConsent(String mrn){
+        String system = "http://www.example.com/random-mrns";
+        Bundle patientSearchResponse;
+        Bundle consentSearchResponse;
 
-        return null;
+        patientSearchResponse = fhirClient.search()
+                .forResource(Patient.class)
+                .where(new TokenClientParam("identifier")
+                        .exactly()
+                        .systemAndCode(system, mrn))
+                .returnBundle(Bundle.class)
+                .execute();
+
+        if(patientSearchResponse == null || patientSearchResponse.getEntry().size() < 1){
+            throw new PatientNotFound("No Patient found for the given MRN:" + mrn);
+        }
+
+        if(patientSearchResponse.getEntry().size() > 1){
+            throw new MultiplePatientsFound("Multiple Patients found for the given MRN:" + mrn);
+        }
+
+        String patientResourceId = patientSearchResponse.getEntry().get(0).getResource().getIdElement().getIdPart();
+
+        consentSearchResponse = fhirClient.search()
+                .forResource(Consent.class)
+                .where(new ReferenceClientParam("patient")
+                        .hasId(patientResourceId))
+                .returnBundle(Bundle.class)
+                .execute();
+
+        if(consentSearchResponse == null || consentSearchResponse.getEntry().size() < 1){
+            throw new ConsentNotFound("No Consent found for the given MRN" + mrn);
+        }
+
+        return consentSearchResponse;
     }
 
 }
