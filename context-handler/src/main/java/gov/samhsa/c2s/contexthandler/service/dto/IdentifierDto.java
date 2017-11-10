9a0edf7303e72538470d6ddf2d8304944145c9d9
burcakulug <burcak.ulug@feisystems.com>
2017-11-10 11:19:34 -0500

    Utilize FullIdentifierBuilder to generate full identifier
    #16588:[Basketball]: As a non-NPI provider, I can be selected on a segmentation request, so I can be part of the consent for data exchange in the C2S system.

--- context-handler/src/main/java/gov/samhsa/c2s/contexthandler/service/dto/IdentifierDto.java
+++ context-handler/src/main/java/gov/samhsa/c2s/contexthandler/service/dto/IdentifierDto.java
@@ -1,11 +1,11 @@
 package gov.samhsa.c2s.contexthandler.service.dto;
 
+import gov.samhsa.c2s.common.util.FullIdentifierBuilder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotBlank;
-import org.springframework.util.Assert;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -28,8 +28,6 @@ public class IdentifierDto {
     }
 
     public String getFullIdentifier() {
-        Assert.hasText(oid, "oid must exist");
-        Assert.hasText(value, "value must exist");
-        return value + "^^^&" + oid + "&ISO";
+        return FullIdentifierBuilder.buildFullIdentifier(value, oid);
     }
 }
